28c6e01c6e638cc4403c8bf8378b9db60e2a66a5
Manish <manish@lapsusnext.com>
2026-04-14 03:11:26 +0530

    fixes

--- src/instrument.ts
+++ src/instrument.ts
@@ -10,7 +10,6 @@ import {
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   integrations: [
-    Sentry.browserTracingIntegration(),
     Sentry.replayIntegration({
       maskAllText: true,
       blockAllMedia: true,
@@ -25,8 +24,6 @@ Sentry.init({
   ],
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
   // Session Replay
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
