cfd2af7d710f556b431235c6845e730d619095fb
Manish <manish@lapsusnext.com>
2026-04-14 03:03:06 +0530

    sentry added

---
+++ src/instrument.ts
@@ -0,0 +1,34 @@
+import * as Sentry from "@sentry/react";
+import React from "react";
+import {
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router-dom";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  enabled: import.meta.env.PROD,
+});
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState, useRef } from "react";
+
 import { useNavigate } from "react-router-dom";
 import {
   User,
